5a9ace689b72204b7eb1c812adfd895faed95daf
fortesaab <fortesa.abdullahuu@gmail.com>
2025-04-01 10:19:47 -0700

    shtova klasat log dhe base entity

---
+++ backend/backend/Core/Entities/Log.cs
@@ -0,0 +1,8 @@
+﻿namespace backend.Core.Entities
+{
+    public class Log : BaseEntity<int>
+    {
+        public string? UserName { get; set; }
+        public string Description { get; set; }
+    }
+}
